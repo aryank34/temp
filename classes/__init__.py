709b046a887f4e4ea118f38a2de30404cfde5bab
Aryan A Kumar <98894571+aryank34@users.noreply.github.com>
2024-02-12 22:46:12 +0530

    Update __init__.py

--- classes/__init__.py
+++ classes/__init__.py
@@ -19,6 +19,6 @@ def create_app():
 
     #register the routes
     app.register_blueprint(loginView)
-    app.register_blueprint(employeeDataView, url_prefix='/dashboard/')
+    app.register_blueprint(employeeDataView)
 
     return app
