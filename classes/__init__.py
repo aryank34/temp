28cd7b4f62ea5325785ca23144ce3aa58481bbe5
Aryan A Kumar <98894571+aryank34@users.noreply.github.com>
2024-02-12 20:39:20 +0530

    Add files via upload

---
+++ classes/__init__.py
@@ -0,0 +1,24 @@
+#helps to convert this folder into a package to import 
+#whatever in this file will run automatically when the folder is imported
+
+from flask import Flask
+
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
+secret_key = os.environ.get("SECRET_KEY")
+
+def create_app():
+    app = Flask(__name__)
+    #used to encrypt or secure the cookies in the session data of app
+    app.config['SECRET KEY'] = secret_key 
+
+    #import the routes
+    from .loginView import loginView
+    from .employeeDataView import employeeDataView
+
+    #register the routes
+    app.register_blueprint(loginView)
+    app.register_blueprint(employeeDataView, url_prefix='/dashboard/')
+
+    return app
---
+++ main.py
@@ -0,0 +1,24 @@
+#to run the server
+
+#this file will import the classes package, can __inti__.py file will automatically run and create an app
+
+from classes import create_app
+
+#allow CORS and give the frontend address to become the whitelist
+from flask_cors import CORS
+
+#load dotenv
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+frontend_url = os.environ.get("FRONTEND")
+
+app = create_app()
+
+# give * for allowing all the urls, give frontend url in production
+CORS(app, origins={frontend_url})
+
+if __name__ == '__main__':
+    app.run(debug = True)
