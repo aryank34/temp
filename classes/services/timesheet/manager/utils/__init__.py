f48717eaa0c0648b3ab9aeacede7fd2609acfb29
Manimit Haldar <manoranjanhalder2013@gmail.com>
2024-02-26 17:26:57 +0530

    push update

--- classes/services/timesheet/manager/utils/__init__.py
+++ classes/services/timesheet/manager/utils/__init__.py
@@ -832,6 +832,10 @@ def create_timesheet(manager_uuid, timesheet):
                     if not verify.status_code == 200:
                         # If the connection fails, return the error response
                         return verify
+                    # check if manager has access to this project
+                    projects = client.WorkBaseDB.Projects.find_one({'_id': ObjectId(timesheet['projectID'])},{"_id": 1,"managerID": 1})
+                    if manager_id != projects['managerID']:
+                        return make_response(jsonify({"error": "Manager doesnt has access to this project"}), 400)
                 else:
                     return make_response(jsonify({"error": "Project is required to create Timesheet"}), 400)
                 
